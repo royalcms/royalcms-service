63e4f80f9c557ec9e67e965cd46ea4d946a3222d
王正东 <wangzhengdong@ecmoban.com>
2020-11-11 10:21:28 +0800

    添加IDE注释

--- Royalcms/Component/Service/Facades/Service.php
+++ Royalcms/Component/Service/Facades/Service.php
@@ -6,6 +6,14 @@ use Royalcms\Component\Support\Facades\Facade;
 
 /**
  * @see \Royalcms\Component\Service\ServiceManager
+ * @method static addService($tag, $app, $handle)
+ * @method static getHandles()
+ * @method static getHandleWithTag($tag)
+ * @method static getHandleWithTagApp($tag, $app)
+ * @method static hasHandleWithTag($tag, $app = null)
+ * @method static fire($tag, $app, $params = array()) 指定一个tag和一个app调用服务
+ * @method static fires($tag, $params = array()) 指定一个tag调用一组服务
+ * @method static handle($handle, $params = array())
  */
 class Service extends Facade
 {
